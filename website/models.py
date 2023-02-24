17a2af87830ddffc76364d40defbab63c8e6131e
Matt <mattmill1217@yahoo.com>
2023-02-23 22:35:21 -0600

    added the db model for favorite database

--- website/models.py
+++ website/models.py
@@ -1,9 +1,16 @@
 from . import db 
 from flask_login import UserMixin 
+from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150)) 
+    first_name = db.Column(db.String(150))
+    favorites = db.relationship('Favorite')
     
+class Favorite(db.Model):
+    imdb_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    data_added = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
